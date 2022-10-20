e0f8cdb73551bc716928f4f6c33df5936f580d12
Mossa <cgmossa@gmail.com>
2022-10-20 20:43:54 +0200

    maybe this rewrite and the use of `(origin, target)` followed by `(target, origin)`  could be more correct

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,36 @@
 use std::collections;
 
-use bevy::{math::DVec2, prelude::*};
+use bevy::{diagnostic::LogDiagnosticsPlugin, math::DVec2, prelude::*, time::FixedTimestep};
 use itertools::Itertools;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_startup_system(add_initial_objects)
+        //TODO: add `Acceleration` to the initial objects.. And maybe `Jerk`.
+        .add_startup_stage_after(
+            StartupStage::Startup,
+            "amend",
+            SystemStage::single_threaded().with_system(amend_objects),
+        )
         .add_system(update_acceleration)
         .add_system(update_forward_euler.after(update_acceleration))
-        .add_system_to_stage(CoreStage::PostUpdate, print_status)
+        .add_stage_after(
+            CoreStage::Last,
+            "info",
+            SystemStage::single_threaded().with_run_criteria(FixedTimestep::step(1.)),
+        )
+        .add_system_to_stage("info", print_status)
         .run()
 }
 
 fn print_status() {
-
+    /*
+    Conserved quantities:
+    1. Momentum
+    2. Angular Momentum
+    3. Energy
+    */
 }
 
 fn add_initial_objects(mut commands: Commands) {
@@ -63,27 +79,41 @@ fn add_initial_objects(mut commands: Commands) {
     ));
 }
 
+fn amend_objects(mut commands: Commands, q_objects: Query<Entity, With<Position>>) {
+    q_objects.for_each(|entity| {
+        commands
+            .entity(entity)
+            .insert((Acceleration::zero(), Jerk::zero()));
+    });
+}
+
 #[derive(Debug, Component)]
 struct Mass(f64);
+
 impl Mass {
     fn new(arg: f64) -> Self {
         Self(arg)
     }
 }
+
 #[derive(Debug, Component)]
 struct Position(DVec2);
+
 impl Position {
     fn new_x(arg: f64) -> Self {
         Self(DVec2::new(arg, 0.))
     }
 }
+
 #[derive(Debug, Component)]
 struct Velocity(DVec2);
+
 impl Velocity {
     fn new_y(arg: f64) -> Self {
         Self(DVec2::new(0., arg))
     }
 }
+
 #[derive(Debug, Clone, Component)]
 struct Acceleration(DVec2);
 
@@ -93,35 +123,31 @@ impl Acceleration {
     }
 }
 
+#[derive(Debug, Clone, Component)]
+struct Jerk(DVec2);
+impl Jerk {
+    fn zero() -> Self {
+        Self(DVec2::ZERO)
+    }
+}
+
 const GRAVITY_CONSTANT: f64 = 6.67e-11;
 
-fn update_acceleration(
-    q_objects: Query<(Entity, &Mass, &Position)>,
-    mut q_acceleration: Query<(Entity, &mut Acceleration)>,
-) {
-    // calculate the new acceleration
-    let new_acceleration: collections::HashMap<Entity, Acceleration> = q_objects
-        .iter_combinations::<2>()
-        .into_grouping_map_by(|[origin, _target]| -> Entity { origin.0 })
-        .fold(
-            Acceleration::zero(),
-            |mut acc, &origin_entity, [origin, target]| {
-                let target_entity: Entity = target.0;
-                if origin_entity == target_entity {
-                    return acc;
-                }
-                let target_mass: &Mass = target.1;
-                // let target_position: &Position = target.2;
-                let direction = origin.2 .0 - target.2 .0;
-                let distance = direction.length().powi(3);
-                acc.0 -= GRAVITY_CONSTANT * target_mass.0 * direction / distance;
-                acc
-            },
-        );
-    // apply
-    q_acceleration.for_each_mut(|(entity, mut acc)| {
-        acc.0 = new_acceleration[&entity].0;
-    })
+fn update_acceleration(mut q_objects: Query<(Entity, &Mass, &Position, &mut Acceleration)>) {
+    // reset acceleration
+    q_objects.for_each_mut(|x| {
+        let mut acceleration: Mut<Acceleration> = x.3;
+        acceleration.0 = DVec2::ZERO;
+    });
+
+    while let Some([mut origin, mut target]) = q_objects.iter_combinations_mut().fetch_next() {
+        if origin.0 == target.0 {
+            panic!("should this happen?")
+        }
+        let offset = origin.2 .0 - target.2 .0;
+        origin.3 .0 -= GRAVITY_CONSTANT * target.1 .0 * offset / offset.length().powi(3);
+        target.3 .0 += GRAVITY_CONSTANT * origin.1 .0 * offset / offset.length().powi(3);
+    }
 }
 
 fn update_forward_euler(
@@ -135,4 +161,11 @@ fn update_forward_euler(
         pos.0 += vel.0 * delta_t;
         vel.0 += acc.0 * delta_t;
     });
+}
+
+fn update_jerk(
+    q_objects: Query<(Entity, &Mass, &Position, &Velocity)>,
+    q_jerk: Query<(Entity, &mut Jerk)>,
+) {
+    todo!()
 }
